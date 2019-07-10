579ec1c11301d5b8abae68aca2468ccfa07c89e2
kimjunhee <developer9101@gmail.com>
2019-07-10 15:25:50 +0900

    props 사용, 하위 태그에 props 전달

--- src/Movie.js
+++ src/Movie.js
@@ -3,10 +3,11 @@ import './Movie.css';
 
 class Movie extends Component {
     render() {
+        console.log(this.props);
         return (
             <div>
-                <MoviePoster />
-                <h1>hello this is a movie</h1>
+                <h1>{this.props.title}</h1>
+                <MoviePoster poster={this.props.poster} />
             </div>
         );
     }
@@ -15,7 +16,7 @@ class Movie extends Component {
 class MoviePoster extends Component{
     render() {
         return (
-            <img src="https://scontent.ficn6-1.fna.fbcdn.net/v/t1.0-9/47579532_581134639006795_3584058279031996416_o.jpg?_nc_cat=107&_nc_oc=AQlNRt6QIYS52V7lWl8_Uz_Gtbl8mVtCn8YpgR-wDh5s9Lbtl9q5fvByW_w-prNA3NA&_nc_ht=scontent.ficn6-1.fna&oh=e2a6a41ac75c97bdfce8f3a15529e795&oe=5DC62D08" />
+            <img src={this.props.poster} />
         );
     }
 }
