fd1f48879ba9ebd4ae8d780e652d67281889f6e5
kimjunhee <developer9101@gmail.com>
2019-07-10 17:00:39 +0900

    state 사용 ( state 안의 변수가 변경될경우 render 재실행, 기존 array에서 추가할경우 ... 사용

--- src/App.js
+++ src/App.js
@@ -2,25 +2,6 @@ import React, {Component} from 'react';
 import './App.css';
 import Movie from './Movie.js';
 
-const movies = [
-  {
-    title: "모어 댄 블루",
-    poster: "https://movie-phinf.pstatic.net/20181128_141/1543382638212PApzR_JPEG/movie_image.jpg?type=m665_443_2"
-  },
-  {
-    title: "토이스토리4",
-    poster: "https://movie-phinf.pstatic.net/20190611_117/1560221190708y3bbv_JPEG/movie_image.jpg?type=m665_443_2"
-  },
-  {
-    title: "알라딘",
-    poster: "https://movie-phinf.pstatic.net/20190524_195/1558663257252zJSKg_JPEG/movie_image.jpg?type=m665_443_2"
-  },
-  {
-    title: "스파이더맨: 파 프롬 홈",
-    poster: "https://movie-phinf.pstatic.net/20190603_165/1559539749502u3EEh_JPEG/movie_image.jpg?type=m665_443_2"
-  }
-]
-
 class App extends Component {
   // Render: componentWillMount() -> render() -> componentDidMount() / component 가 존재할경우
   // Update componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> component
@@ -31,6 +12,38 @@ class App extends Component {
 
   componentDidMount(){
     console.log("3.did mount");
+    setTimeout(() => {
+      this.setState({
+        movies: [
+            ...this.state.movies,
+          {
+            title: "존윅3",
+            poster: "https://movie-phinf.pstatic.net/20190626_170/1561524289335uILG7_JPEG/movie_image.jpg?type=m665_443_2"
+          }
+        ]
+      })
+    }, 2000)
+  }
+
+  state = {
+    movies: [
+      {
+        title: "모어 댄 블루",
+        poster: "https://movie-phinf.pstatic.net/20181128_141/1543382638212PApzR_JPEG/movie_image.jpg?type=m665_443_2"
+      },
+      {
+        title: "토이스토리4",
+        poster: "https://movie-phinf.pstatic.net/20190611_117/1560221190708y3bbv_JPEG/movie_image.jpg?type=m665_443_2"
+      },
+      {
+        title: "알라딘",
+        poster: "https://movie-phinf.pstatic.net/20190524_195/1558663257252zJSKg_JPEG/movie_image.jpg?type=m665_443_2"
+      },
+      {
+        title: "스파이더맨: 파 프롬 홈",
+        poster: "https://movie-phinf.pstatic.net/20190603_165/1559539749502u3EEh_JPEG/movie_image.jpg?type=m665_443_2"
+      }
+    ]
   }
 
 
@@ -38,7 +51,7 @@ class App extends Component {
     console.log("2.render");
     return (
         <div className="App">
-          {movies.map((movie, index) => {
+          {this.state.movies.map((movie, index) => {
             return (
                 <Movie title={movie.title} poster={movie.poster} key={index} />
             );
