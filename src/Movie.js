ef93a1c9f8b447a3c3d1a5c51396933433bc5a77
kimjunhee <developer9101@gmail.com>
2019-07-11 12:30:24 +0900

    라이프사이클이 필요없는경우(단순리턴) class를 사용할 필요없이 function으로 가능, class에서 사용하는 Component를 지우고, state도 지운다, propTypes는 functionName.propTypes로 사용가능

--- src/Movie.js
+++ src/Movie.js
@@ -1,37 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-class Movie extends Component {
-    //yarn add prop-types 사용, proptype을 사용해서 부모컴포넌트에서 받아오는값, 타입을 지정할수있다 혹은 받아와도 사용안할경우 오류남.
-    //isRequired: 컴포넌트값에 prop을 제공하는걸 필수로 지정
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        poster: PropTypes.string.isRequired
-    }
-
-    render() {
-        console.log(this.props);
-        return (
-            <div>
-                <h1>{this.props.title}</h1>
-                <MoviePoster poster={this.props.poster} />
-            </div>
-        );
-    }
+function Movie({title, poster}){
+    console.log('title: '+title+' >>> poster : '+poster);
+    return (
+        <div>
+            <h1>{title}</h1>
+            <MoviePoster poster={poster} />
+        </div>
+    )
 }
 
-class MoviePoster extends Component{
+function MoviePoster({poster}){
+    return (
+        <img src={poster} alt="movie poster"/>
+    )
+}
 
-    static propTypes = {
-        poster: PropTypes.string.isRequired
-    }
+Movie.propTypes = {
+    //yarn add prop-types 사용, proptype을 사용해서 부모컴포넌트에서 받아오는값, 타입을 지정할수있다 혹은 받아와도 사용안할경우 오류남.
+    //isRequired: 컴포넌트값에 prop을 제공하는걸 필수로 지정
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
+}
 
-    render() {
-        return (
-            <img src={this.props.poster} />
-        );
-    }
+MoviePoster.propTypes = {
+    poster: PropTypes.string.isRequired
 }
 
 export default Movie;
